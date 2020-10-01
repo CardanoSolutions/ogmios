0cf51c8bfcb7f26600f8d78adfab5a00482caaae
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-10-01 22:13:31 +1000

    fix: update demo to address a few issues in the Cardano era - use local server - rename page title - use blockNo instead of slot and epoch for simplicity

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("wss://ogmios.dev");
+const socket = new WebSocket("ws://127.0.0.1:1337");
 
 socket.onopen = function (event) {
     socket.wsp("FindIntersect", { points: ["origin"] }, { step: "INIT" });
