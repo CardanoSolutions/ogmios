e0c5d5bd7e26ed8eaa301ad91bfb1f02d453a84c
KtorZ <matthias.benkort@gmail.com>
2021-02-06 03:00:23 +0100

    make JSON prelude primitives as 'INLINABLE'.

--- ogmios-server/src/Ogmios/Data/Json/Prelude.hs
+++ ogmios-server/src/Ogmios/Data/Json/Prelude.hs
@@ -141,113 +141,142 @@ keepRedundantConstraint _ = ()
 encodeBlockNo :: BlockNo -> Json
 encodeBlockNo =
     encodeWord64 . unBlockNo
+{-# INLINABLE encodeBlockNo #-}
 
 encodeBool :: Bool -> Json
-encodeBool = Json.Bool
+encodeBool =
+    Json.Bool
+{-# INLINABLE encodeBool #-}
 
 encodeByteArray :: ByteArrayAccess ba => (ByteString -> Json) -> ba -> Json
 encodeByteArray encodeByteString =
     encodeByteString . BA.convert
+{-# INLINABLE encodeByteArray #-}
 
 encodeByteStringBase16 :: ByteString -> Json
 encodeByteStringBase16 =
     encodeText . encodeBase16
+{-# INLINABLE encodeByteStringBase16 #-}
 
 encodeByteStringBech32 :: HumanReadablePart -> ByteString -> Json
 encodeByteStringBech32 hrp =
     encodeText . Bech32.encodeLenient hrp . Bech32.dataPartFromBytes
+{-# INLINABLE encodeByteStringBech32 #-}
 
 encodeByteStringBase64 :: ByteString -> Json
 encodeByteStringBase64 =
     encodeText . encodeBase64
+{-# INLINABLE encodeByteStringBase64 #-}
 
 encodeDnsName :: DnsName -> Json
 encodeDnsName =
     encodeText . dnsToText
+{-# INLINABLE encodeDnsName #-}
 
 encodeDouble :: Double -> Json
 encodeDouble =
     Json.toJSON
+{-# INLINABLE encodeDouble #-}
 
 encodeEpochNo :: EpochNo -> Json
 encodeEpochNo =
     encodeWord64 . unEpochNo
+{-# INLINABLE encodeEpochNo #-}
 
 encodeIPv4 :: IPv4 -> Json
 encodeIPv4 =
     encodeString . show
+{-# INLINABLE encodeIPv4 #-}
 
 encodeIPv6 :: IPv6 -> Json
 encodeIPv6 =
     encodeString . show
+{-# INLINABLE encodeIPv6 #-}
 
 encodeInteger :: Integer -> Json
 encodeInteger =
     encodeScientific . fromInteger
+{-# INLINABLE encodeInteger #-}
 
 encodeNatural :: Natural -> Json
 encodeNatural =
     encodeInteger . toInteger
+{-# INLINABLE encodeNatural #-}
 
 encodeNull :: Json
 encodeNull =
     Json.Null
+{-# INLINABLE encodeNull #-}
 
 encodePort :: Port -> Json
 encodePort =
     encodeWord16 . portToWord16
+{-# INLINABLE encodePort #-}
 
 encodeRational :: Rational -> Json
 encodeRational r =
     encodeText (show (numerator r) <> "/" <> show (denominator r))
+{-# INLINABLE encodeRational #-}
 
 encodeScientific :: Scientific -> Json
 encodeScientific =
     Json.Number
+{-# INLINABLE encodeScientific #-}
 
 encodeShortByteString :: (ByteString -> Json) -> ShortByteString -> Json
 encodeShortByteString encodeByteString =
     encodeByteString . fromShort
+{-# INLINABLE encodeShortByteString #-}
 
 encodeSlotNo :: SlotNo -> Json
 encodeSlotNo =
     encodeWord64 . unSlotNo
+{-# INLINABLE encodeSlotNo #-}
 
 encodeString :: String -> Json
 encodeString =
     encodeText . toText
+{-# INLINABLE encodeString #-}
 
 encodeText :: Text -> Json
 encodeText =
     Json.String
+{-# INLINABLE encodeText #-}
 
 encodeUnitInterval :: UnitInterval -> Json
 encodeUnitInterval =
     encodeRational . unitIntervalToRational
+{-# INLINABLE encodeUnitInterval #-}
 
 encodeUrl :: Url -> Json
 encodeUrl =
     encodeText . urlToText
+{-# INLINABLE encodeUrl #-}
 
 encodeWord :: Word -> Json
 encodeWord =
     Json.toJSON
+{-# INLINABLE encodeWord #-}
 
 encodeWord8 :: Word8 -> Json
 encodeWord8 =
     Json.toJSON
+{-# INLINABLE encodeWord8 #-}
 
 encodeWord16 :: Word16 -> Json
 encodeWord16 =
     Json.toJSON
+{-# INLINABLE encodeWord16 #-}
 
 encodeWord32 :: Word32 -> Json
 encodeWord32 =
     Json.toJSON
+{-# INLINABLE encodeWord32 #-}
 
 encodeWord64 :: Word64 -> Json
 encodeWord64 =
     Json.toJSON
+{-# INLINABLE encodeWord64 #-}
 
 --
 -- Data-Structures
@@ -256,10 +285,12 @@ encodeWord64 =
 encodeAnnotated :: (a -> Json) -> Annotated a any -> Json
 encodeAnnotated encodeElem =
     encodeElem . unAnnotated
+{-# INLINABLE encodeAnnotated #-}
 
 encodeIdentity :: (a -> Json) -> Identity a -> Json
 encodeIdentity encodeElem =
     encodeElem . runIdentity
+{-# INLINABLE encodeIdentity #-}
 
 encodeFoldable :: Foldable f => (a -> Json) -> f a -> Json
 encodeFoldable encodeElem =
@@ -269,22 +300,27 @@ encodeFoldable encodeElem =
 {-# SPECIALIZE encodeFoldable :: (a -> Json) -> Vector a -> Json #-}
 {-# SPECIALIZE encodeFoldable :: (a -> Json) -> Set a -> Json #-}
 {-# SPECIALIZE encodeFoldable :: (a -> Json) -> StrictSeq a -> Json #-}
+{-# INLINABLE encodeFoldable #-}
 
 encodeList :: (a -> Json) -> [a] -> Json
 encodeList encodeElem =
     Json.toJSON . fmap encodeElem
+{-# INLINABLE encodeList #-}
 
 encodeMap :: (k -> Text) -> (v -> Json) -> Map k v -> Json
 encodeMap encodeKey encodeValue =
     encodeObject . Map.foldrWithKey (\k v -> (:) (encodeKey k, encodeValue v)) []
+{-# INLINABLE encodeMap #-}
 
 encodeMaybe :: (a -> Json) -> Maybe a -> Json
 encodeMaybe =
     maybe encodeNull
+{-# INLINABLE encodeMaybe #-}
 
 encodeObject :: [(Text, Json)] -> Json
 encodeObject =
     Json.object
+{-# INLINABLE encodeObject #-}
 
 encode2Tuple
     :: (a -> Json)
@@ -293,6 +329,7 @@ encode2Tuple
     -> Json
 encode2Tuple encodeA encodeB (a,b) =
     Json.toJSON [encodeA a, encodeB b]
+{-# INLINABLE encode2Tuple #-}
 
 encode3Tuple
     :: (a -> Json)
@@ -302,6 +339,7 @@ encode3Tuple
     -> Json
 encode3Tuple encodeA encodeB encodeC (a, b, c) =
     Json.toJSON [encodeA a, encodeB b, encodeC c]
+{-# INLINABLE encode3Tuple #-}
 
 encode4Tuple
     :: (a -> Json)
