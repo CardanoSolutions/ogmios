abb1594c40483d26489b42da1d171fe13da9daf5
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2021-03-15 13:48:42 +1100

    feature: add type guard utility functions for block introspection

--- client/TypeScript/src/util.ts
+++ client/TypeScript/src/util.ts
@@ -1,5 +1,12 @@
 import { InteractionContext } from './Connection'
-import { Point } from './schema'
+import {
+  Block,
+  BlockAllegra,
+  BlockByron,
+  BlockMary,
+  BlockShelley,
+  Point
+} from './schema'
 import { findIntersect } from './ChainSync'
 import { TipIsOriginError } from './errors'
 
@@ -13,3 +20,15 @@ export const createPointFromCurrentTip = async (context?: InteractionContext): P
     slot: tip.slot
   } as Point
 }
+
+export const isAllegraBlock = (block: Block): block is { allegra: BlockAllegra } =>
+  (block as { allegra: BlockAllegra }).allegra !== undefined
+
+export const isByronBlock = (block: Block): block is { byron: BlockByron } =>
+  (block as { byron: BlockByron }).byron !== undefined
+
+export const isMaryBlock = (block: Block): block is { mary: BlockMary } =>
+  (block as { mary: BlockMary }).mary !== undefined
+
+export const isShelleyBlock = (block: Block): block is { shelley: BlockShelley } =>
+  (block as { shelley: BlockShelley }).shelley !== undefined
