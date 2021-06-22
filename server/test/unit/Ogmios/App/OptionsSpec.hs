d61bc2090e8defd0d9e7bb847263b94e421494be
KtorZ <matthias.benkort@gmail.com>
2021-06-22 21:44:31 +0200

    Write basic unit test for 'parseNetworkParameters'

--- server/test/unit/Ogmios/App/OptionsSpec.hs
+++ server/test/unit/Ogmios/App/OptionsSpec.hs
@@ -9,25 +9,37 @@ module Ogmios.App.OptionsSpec
 import Ogmios.Prelude
 
 import Ogmios.App.Options
-    ( NetworkParameters (..) )
-
-import Test.Hspec
-    ( Spec, context, parallel )
-import Test.Hspec.QuickCheck
-    ( prop )
-import Test.QuickCheck
-    ( Arbitrary (..)
-    , Gen
-    , Property
-    , choose
-    , conjoin
-    , elements
-    , forAll
-    , property
-    , (===)
+    ( EpochSlots (..)
+    , NetworkMagic (..)
+    , NetworkParameters (..)
+    , mkSystemStart
+    , parseNetworkParameters
     )
+import Test.Hspec
+    ( Spec, context, parallel, shouldBe, specify )
+
+import qualified Paths_ogmios
 
 spec :: Spec
 spec = parallel $ do
     context "parseNetworkParameters" $ do
-        prop "TODO" (property True)
+        specify "mainnet" $ do
+            params <- parseNetworkParameters =<< getConfigFile "mainnet"
+            networkMagic  params `shouldBe` NetworkMagic 764824073
+            systemStart   params `shouldBe` mkSystemStart 1506203091
+            slotsPerEpoch params `shouldBe` EpochSlots 432000
+
+        specify "testnet" $ do
+            params <- parseNetworkParameters =<< getConfigFile "testnet"
+            networkMagic  params `shouldBe` NetworkMagic 1097911063
+            systemStart   params `shouldBe` mkSystemStart 1563999616
+            slotsPerEpoch params `shouldBe` EpochSlots 432000
+
+--
+-- Helper
+--
+
+getConfigFile :: String -> IO FilePath
+getConfigFile network =
+    "config/network/" <> network <> "/cardano-node/config.json"
+    & Paths_ogmios.getDataFileName
