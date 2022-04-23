005c7a684139ad464f25d9144b9e2ed5de36c770
KtorZ <matthias.benkort@gmail.com>
2022-04-23 17:02:44 +0200

    Test both submitTx and the submission client in the TypeScript specs.

--- clients/TypeScript/packages/client/test/TxSubmission/TxSubmission.test.ts
+++ clients/TypeScript/packages/client/test/TxSubmission/TxSubmission.test.ts
@@ -1,5 +1,5 @@
 import { TxIn, TxOut, Utxo } from '@cardano-ogmios/schema'
-import { InteractionContext, TxSubmission, UnknownResultError } from '../../src'
+import { createTxSubmissionClient, InteractionContext, TxSubmission, UnknownResultError } from '../../src'
 import { dummyInteractionContext } from '../util'
 
 describe('TxSubmission', () => {
@@ -30,18 +30,50 @@ describe('TxSubmission', () => {
     beforeAll(async () => { context = await dummyInteractionContext() })
     afterAll(() => context.socket.close())
 
-    it('rejects with an array of named errors', async () => {
+    const someTransaction =
+      '83a40081825820e1e86da6446c7f81da8d5e440bb0d4eed0f1530ba15bf77e49c33d' +
+      '6f050d8fb500018182581d60ff7b4521589238cfb9c26870edfa782541e615444744' +
+      '22d849ceb1031a001954ce021a000297d9031a05f5e100a10081825820cf14d1c834' +
+      'cecab8e1f5447bde551946804057332825e26e64ee43079dd408355840247c5e6092' +
+      '1130fa1df800d310f39788f4ae04837534ade6727875dbb87218f5b45e96ccd125a1' +
+      '4c4510e81694e7aad3ba8a24458aaf6b6f9c4f1a4801beba05f6'
+
+    // NOTE:
+    //
+    // Used this one to test https://github.com/CardanoSolutions/ogmios/issues/197
+    // The issue was only reproducible is the server took long-enough to submit transactions, so to test
+    // this, I've used an altered version of the server with an artificial delay added to the submission.
+    //
+    // Without this altered server, this test is relatively pointless, hence 'xit'.
+    xit('many transactions', async () => {
+      const client = await createTxSubmissionClient(context)
+      const requests = []
+      for (let i = 0; i < 20; i += 1) {
+        requests.push(client.submitTx(someTransaction).catch((e) => e))
+      }
+      await Promise.all(requests)
+    })
+
+    it('rejects with an array of named errors (submitTx)', async () => {
       try {
-        await TxSubmission.submitTx(
-          context,
-          ('83a40081825820e1e86da6446c7f81da8d5e440bb0d4eed0f1530ba15bf77e49c33d' +
-           '6f050d8fb500018182581d60ff7b4521589238cfb9c26870edfa782541e615444744' +
-           '22d849ceb1031a001954ce021a000297d9031a05f5e100a10081825820cf14d1c834' +
-           'cecab8e1f5447bde551946804057332825e26e64ee43079dd408355840247c5e6092' +
-           '1130fa1df800d310f39788f4ae04837534ade6727875dbb87218f5b45e96ccd125a1' +
-           '4c4510e81694e7aad3ba8a24458aaf6b6f9c4f1a4801beba05f6'
-          )
-        )
+        await TxSubmission.submitTx(context, someTransaction)
+      } catch (error) {
+        await expect(error).toHaveLength(2)
+        // NOTE: We can't predict in which order will the server return the errors.
+        try {
+          await expect(error[0]).toBeInstanceOf(TxSubmission.submissionErrors.errors.BadInputs.Error)
+          await expect(error[1]).toBeInstanceOf(TxSubmission.submissionErrors.errors.ValueNotConserved.Error)
+        } catch (e) {
+          await expect(error[1]).toBeInstanceOf(TxSubmission.submissionErrors.errors.BadInputs.Error)
+          await expect(error[0]).toBeInstanceOf(TxSubmission.submissionErrors.errors.ValueNotConserved.Error)
+        }
+      }
+    })
+
+    it('rejects with an array of named errors (TxSubmissionClient)', async () => {
+      const client = await createTxSubmissionClient(context)
+      try {
+        await client.submitTx(someTransaction)
       } catch (error) {
         await expect(error).toHaveLength(2)
         // NOTE: We can't predict in which order will the server return the errors.
@@ -55,6 +87,7 @@ describe('TxSubmission', () => {
       }
     })
   })
+
   describe('evaluateTx', () => {
     let context: InteractionContext
     beforeAll(async () => { context = await dummyInteractionContext() })
