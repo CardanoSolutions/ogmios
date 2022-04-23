6a41f06a207c73a0c58494838ee7af74789c9369
KtorZ <matthias.benkort@gmail.com>
2022-04-23 16:53:28 +0200

    Rework submitTx to use the new util for waiting on server responses.
      Now, we only need to have a single event listener for the whole submission client. Many requests can be fired asynchronously and handled in due time; behind the scene the server still process them one at the time though.

--- clients/TypeScript/packages/client/src/TxSubmission/TxSubmissionClient.ts
+++ clients/TypeScript/packages/client/src/TxSubmission/TxSubmissionClient.ts
@@ -1,7 +1,9 @@
 import { InteractionContext } from '../Connection'
-import { ensureSocketIsOpen } from '../util'
-import { submitTx } from './submitTx'
+import { ensureSocketIsOpen, eventEmitterToGenerator, safeJSON } from '../util'
+import { handleSubmitTxResponse, isTxId } from './submitTx'
 import { evaluateTx } from './evaluateTx'
+import { Ogmios, TxId } from '@cardano-ogmios/schema'
+import { baseRequest, send } from '../Request'
 
 /**
  * See also {@link createTxSubmissionClient} for creating a client.
@@ -11,7 +13,7 @@ import { evaluateTx } from './evaluateTx'
 export interface TxSubmissionClient {
   context: InteractionContext
   evaluateTx: (bytes: string) => ReturnType<typeof evaluateTx>
-  submitTx: (bytes: string) => ReturnType<typeof submitTx>
+  submitTx: (bytes: string) => Promise<TxId>
   shutdown: () => Promise<void>
 }
 
@@ -24,15 +26,40 @@ export const createTxSubmissionClient = async (
   context: InteractionContext
 ): Promise<TxSubmissionClient> => {
   const { socket } = context
+
+  const matchSubmitTx = (data: string) => {
+    const response = safeJSON.parse(data) as Ogmios['SubmitTxResponse']
+    if (response.methodname !== 'SubmitTx') {
+      return null
+    }
+    return response
+  }
+  const submitTxResponse = eventEmitterToGenerator(socket, 'message', matchSubmitTx)() as
+    AsyncGenerator<Ogmios['SubmitTxResponse']>
+
   return Promise.resolve({
     context,
     evaluateTx: (bytes) => {
       ensureSocketIsOpen(socket)
       return evaluateTx(context, bytes)
     },
-    submitTx: (bytes) => {
+    submitTx: async (bytes) => {
       ensureSocketIsOpen(socket)
-      return submitTx(context, bytes)
+      return send<TxId>(async (socket) => {
+        socket.send(safeJSON.stringify({
+          ...baseRequest,
+          methodname: 'SubmitTx',
+          args: { submit: bytes }
+        } as unknown as Ogmios['SubmitTx']))
+
+        const response = handleSubmitTxResponse((await submitTxResponse.next()).value)
+
+        if (isTxId(response)) {
+          return response
+        } else {
+          throw response
+        }
+      }, context)
     },
     shutdown: () => new Promise(resolve => {
       ensureSocketIsOpen(socket)
